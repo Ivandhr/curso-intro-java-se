2550ba15dfc407a7804a55dbe7a956c45c388b31
ivan.hernandez <ivan.hernandez@dautom.com.co>
2023-01-22 18:36:14 -0500

    10. Variables en Java

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variables declaration
+        int speed;
+        //Setting variable value
+        speed = 10;
+        System.out.println(speed);
+        //Variable declaration and setting value
+        int salary = 1000;
+        String employeeName = "Ivan Hernandez";
+        System.out.println(employeeName);
+    }
+}
