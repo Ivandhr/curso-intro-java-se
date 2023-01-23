9d814e485cb7d2cb9b58bd110f28cbe701bba259
ivan.hernandez <ivan.hernandez@dautom.com.co>
2023-01-23 10:50:27 -0500

    12. Convención de Nombres en Java

---
+++ src/NamingJava.java
@@ -0,0 +1,31 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        // Special case for integer number to long.
+        long celphone = 3105555555L;
+        long celPhone = 3103333333L;
+
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Colombia";
+        String _backgroundColor = "Green";
+
+        // This is not correct
+        // String background-Color = "Red";
+
+        String currency$ = "COP";
+        String background_color = "Yellow";
+
+        // Constants
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+
+
+
+
+
+
+    }
+}
