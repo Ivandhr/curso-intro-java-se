c44eeacea1653d0afa2e7490dab1570a53cea6c2
ivan.hernandez <ivan.hernandez@dautom.com.co>
2023-01-29 11:20:07 -0500

    14. Tipos de datos numericos

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Integers
+        byte nB = 127;
+        short bS = 32767;
+        int n = 2147483647;
+        long nL = 9223372036854775807L;
+
+        //Punto Flotante
+        double nD = 123.456;
+        float nF = 123.456F;
+
+
+    }
+}
