2cfcb67a0d39322f07f503e97d02e42107d07918
ivan.hernandez <ivan.hernandez@dautom.com.co>
2023-01-29 13:36:47 -0500

    21. Ejercicio 1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,50 @@
+public class Ejercicio1 {
+
+    /*
+    Ejercicio 1.
+    Ponte creativo y preséntanos a tu familia con variables,
+    utiliza todos los tipos y cantidad de variables que puedas,
+    no olvides las convenciones de nombres Camel Case.
+     */
+
+    public static void main(String[] args) {
+        String wifeName = "Natalia Milena";
+        String wifeLastName = "Duarte Muñoz";
+        int wifeAge = 37;
+
+        String motherName = "Luz Stella";
+        String motherLastName = "Rodriguez Peña";
+        int motherAge = 70;
+
+        String fatherName = "Ivan Ramiro";
+        String fatherLastname = "Hernandez Atehortua";
+        int fatherAge = 70;
+
+        String motherInLawName = "Amanda Margarita";
+        String motherInLawLastName = "Muñoz Jacome";
+        int motherInLawAge = 70;
+
+        String fatherInLawName = "Mario Jose";
+        String fatherInLawLastName = "Duarte Calderon";
+        int fatherInLawAge = 70;
+
+        System.out.println("Mi familia esta conformada por mi esposa, mis papas y mis suegros");
+        System.out.println("Mi esposita linda se llama: " + wifeName + " " + wifeLastName + " y tiene " + wifeAge + " años.");
+        System.out.println("Mi mama se llama: " + motherName + " " + motherLastName + " y tiene " + motherAge + " años.");
+        System.out.println("Mi papa se llama: " + fatherName + " " + fatherLastname + " y tiene " + fatherAge + " años.");
+        System.out.println("Mi suegra se llama: " + motherInLawName + " " + motherInLawLastName + " y tiene " + motherInLawAge + " años.");
+        System.out.println("Mi suegro se llama: " + fatherInLawName + " " + fatherInLawLastName + " y tiene " + fatherInLawAge + " años.");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
